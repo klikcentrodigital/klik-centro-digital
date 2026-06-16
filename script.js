460ef1352a87e93d0b331e58e590341b0d5ab8bf
klikcentrodigital <klikcentrodigital@gmail.com>
2026-06-15 21:02:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+function showTab(tabId, button) {
 
+    const tabs = document.querySelectorAll('.tab-content');
+    const buttons = document.querySelectorAll('.tab-button');
+
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    buttons.forEach(btn => {
+        btn.classList.remove('active');
+    });
+
+    document.getElementById(tabId).classList.add('active');
+
+    button.classList.add('active');
+}
